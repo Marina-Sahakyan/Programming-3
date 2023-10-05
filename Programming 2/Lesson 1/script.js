9ba10a00563448eed5b4d3b6308662cf884a3bb2
Marina-Sahakyan <marina.sahakyan1.y@tumo.org>
2023-10-05 17:23:46 +0400

    initial project

---
+++ Programming 2/Lesson 1/script.js
@@ -0,0 +1,43 @@
+// var a = 100;
+// var b = 200;
+// var p = calcPer(a,b);
+// var s = calcSq(a,b);
+// console.log(p,s);
+
+// function calcPer(c,d){
+//   return c+d
+// }
+
+// function calcSq(e,f){
+//   return e*f
+// }
+
+
+//var c = [45,60,12,98,78,154,65];
+//var m = [15,98,45,33,78,98,100,56,98];
+//var s1 = sumArray(c);
+//var s2 = sumArray(m);
+//console.log(s1, s2);
+
+
+//function sumArray(a){
+//    let sum=0
+//for (let index in a) {
+//  
+//    sum +=a[index]
+//}
+// return sum
+//}
+
+var c = [45,60,12,98,78,154,65];
+var b = c.reverse();
+console.log(b); //պիտի տպի 65,154,78,98,12,60,45
+
+var t = ["php","javascript","html","css","mysql"];
+var m = t.reverse();
+console.log(m); //պիտի տպի mysql, css, html, javascript, php
+
+function reverseArray(a){
+     //write your code here
+  
+}
